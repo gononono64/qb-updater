ad65c4982b31243e1cdae2277e982c6b8b136053
Mr.Cowley <96976330+gononono64@users.noreply.github.com>
2023-06-26 17:58:10 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,9 @@
 fx_version 'cerulean'
 game 'gta5'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'MrCrowley - https://github.com/gononono64 '
 description 'QBCore Resource Updater'
-version '1.0.3'
+version '1.0.4'
 
 server_scripts { 'config.lua' , 'server/server.lua' }
 
